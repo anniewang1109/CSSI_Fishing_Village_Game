6f9942484d8c65add1ff87a9ed579610387ab76f
Demo User <demouser@MacBook-Pro.local>
2018-07-20 16:31:57 -0400

    resolve merge conflict again

--- Main.py
+++ Main.py
@@ -3,11 +3,8 @@ from google.appengine.ext import ndb
 class Fish(ndb.Model):
     level = ndb.IntegerProperty()
     spriteLink = ndb.StringProperty()
-<<<<<<< Updated upstream
 
 class Fisherman(ndb.Model):
     type = ndb.StringProperty()
     spriteLink = ndb.StringProperty()
     fishCaught = ndb.IntegerProperty()
-=======
->>>>>>> Stashed changes
