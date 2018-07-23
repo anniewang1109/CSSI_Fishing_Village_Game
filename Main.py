b8af09c0684e3f23776490298029ed5981f7f5fe
Demo User <demouser@MacBook-Pro.local>
2018-07-23 11:52:25 -0400

    used appengine user api

--- Main.py
+++ Main.py
@@ -11,8 +11,24 @@ jinja_current_directory = jinja2.Environment(
 
 class mainHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_users()
-        
+        user = users.get_current_user()
+        nickname = None
+        login_url = None
+        logout_url = None
+
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+        else:
+            login_url = users.create_login_url('/')
+
+        tempate_vars = {
+            "user": user,
+            "nickname": nickname,
+            "logout_url": logout_url,
+            "login_url": login_url,
+        }
+
         template = jinja_current_directory.get_template('/templates/startPage.html')
         self.response.write(template.render())
     #def post(self):
