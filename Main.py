ae3b2be75e08df69c6a5d9a9c9e0a579155258ea
Demo User <demouser@MacBook-Pro.local>
2018-07-23 09:03:51 -0400

    Added User class

--- Main.py
+++ Main.py
@@ -1,10 +1,22 @@
 from google.appengine.ext import ndb
 
 class Fish(ndb.Model):
-    level = ndb.IntegerProperty()
+    strength = ndb.IntegerProperty()
     spriteLink = ndb.StringProperty()
 
 class Fisherman(ndb.Model):
     type = ndb.StringProperty()
     spriteLink = ndb.StringProperty()
     fishCaught = ndb.IntegerProperty()
+    #xPos = ndb.IntegerProperty()
+    #yPos = ndb.IntegerProperty()
+
+class User(ndb.Model):
+    username = ndb.StringProperty()
+    level = ndb.IntegerProperty()
+    #fishermanPlacement = ndb.KeyProperty(kind = Fisherman, repeated = true)
+
+addNewUser(self, username):
+    if(User.query(username == username).count == 0):
+        new_User = User(username = username, level = 1)
+        new User.put()
