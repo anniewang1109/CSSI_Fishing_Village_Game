533d2566cc0f1890b6bae492ccf8dab5847d4de6
Demo User <demouser@MacBook-Pro.local>
2018-07-23 11:42:07 -0400

    undid the seperation for handlers

--- Main.py
+++ Main.py
@@ -2,12 +2,18 @@ import webapp2
 import jinja2
 import os
 from models import *
-from handlers import *
 
 jinja_current_directory = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class mainHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_current_directory.get_template('/templates/startPage.html')
+        self.response.write(template.render())
+    #def post(self):
+        #Log in or make account
 
 app = webapp2.WSGIApplication([
     ('/', mainHandler),
