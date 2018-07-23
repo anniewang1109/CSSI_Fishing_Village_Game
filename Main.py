2dbfc942ab47dc9b6e1590297cc57030329992b6
Demo User <demouser@MacBook-Pro.local>
2018-07-23 17:38:40 -0400

    Major changes to handlers and start button

--- Main.py
+++ Main.py
@@ -11,10 +11,6 @@ jinja_current_directory = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_current_directory.get_template('/templates/welcomePage.html')
-        self.response.write(template.render())
-    def post(self):
-        console.log("test");
         user = users.get_current_user()
         nickname = None
         login_url = None
@@ -32,9 +28,8 @@ class MainHandler(webapp2.RequestHandler):
             "logout_url": logout_url,
             "login_url": login_url,
         }
-        template = jinja_current_directory.get_template('/templates/gamePage.html')
+        template = jinja_current_directory.get_template('/templates/welcomePage.html')
         self.response.write(template.render())
-        self.response.write("<p>" + tempate_vars['user'] + ", " + tempate_vars['login_url'] +  ", " + tempate_vars['logout_url'] + ", " + tempate_vars['nickname']+ "</p>")
 
     #def post(self):
         #Log in or make account
